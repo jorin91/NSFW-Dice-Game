5cfe7936c2f1e6a13887f2c615959726155b499e
jorin91 <jorin91@hotmail.com>
2025-12-06 00:55:49 +0100

    Refactor initialization sequence to include firebaseWriteTest

--- js/init.js
+++ js/init.js
@@ -1,7 +1,9 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
 import { gameInitFromStorage } from "./gamestate.js";
+import { firebaseWriteTest } from "./firebase/firebase-test.js";
 
 gameInitFromStorage(); // GameState
 initI18n(); // Translation
 initPanelNavigation(); // Navigation
+firebaseWriteTest(); // Firebase Test
