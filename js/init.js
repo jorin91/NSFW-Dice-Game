995ed7805975fea75a4bf51b11877987ee71dd4c
jorin91 <jorin91@hotmail.com>
2025-12-06 00:05:33 +0100

    Import gameInitFromStorage and call it during initialization

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
+import { gameInitFromStorage } from "./gamestate.js";
 
+gameInitFromStorage(); // GameState
 initI18n(); // Translation
 initPanelNavigation(); // Navigation
