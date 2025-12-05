de29cc9265fdd7fcb1126c3d152fbc68a5fca8ba
jorin91 <jorin91@hotmail.com>
2025-12-06 00:55:56 +0100

    Add firebase configuration file

---
+++ js/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCVvF5tEYJ0e5Zr8qBPJY2Da-razKi1Iw",
+    authDomain: "nsfw-dicegame.firebaseapp.com",
+    databaseURL: "https://nsfw-dicegame-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nsfw-dicegame",
+    storageBucket: "nsfw-dicegame.appspot.com",
+    messagingSenderId: "516885154786",
+    appId: "1:516885154786:web:0e56def9aed4d6136e0587bb",
+    measurementId: "G-VBZWD7V71Y" // niet nodig voor DB, is voor Analytics
+};
