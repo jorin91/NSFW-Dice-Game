07064c29402511b4c810de7ec52720ac27dccd78
jorin91 <jorin91@hotmail.com>
2025-12-06 23:15:34 +0100

    Refactor initialization logic to separate setup functions for improved readability and maintainability

--- js/init.js
+++ js/init.js
@@ -1,7 +1,20 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
 import { firebaseWriteTest } from "./firebase/firebase-test.js";
+import { setupPanelPlayer } from "./panel-player.js";
 
-initI18n(); // Translation
-initPanelNavigation(); // Navigation
-await firebaseWriteTest(); // Firebase Test
+export async function init() {
+  // Initialization functions
+  initI18n(); // Translation
+  initPanelNavigation(); // Navigation
+  await firebaseWriteTest(); // Firebase Test
+}
+
+export async function initUpdate() {
+  setupPanelPlayer(); // Setup Player Panel
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await init();
+  await initUpdate();
+});
