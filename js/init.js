3d3b511cf7f6f5e07ca82802ae85878089cfccd2
jorin91 <jorin91@hotmail.com>
2025-12-06 22:12:35 +0100

    Add player initialization to the game setup process

--- js/init.js
+++ js/init.js
@@ -1,7 +1,9 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
 import { firebaseWriteTest } from "./firebase/firebase-test.js";
+import { initPlayer } from "./player.js";
 
 initI18n(); // Translation
 initPanelNavigation(); // Navigation
+initPlayer(); // Player Initialization
 await firebaseWriteTest(); // Firebase Test
