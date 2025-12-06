922f03d2970ce79d6997d13400fa8238a8331a76
jorin91 <jorin91@hotmail.com>
2025-12-06 02:14:23 +0100

    Remove game state initialization from init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,7 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
-import { gameInitFromStorage } from "./gamestate.js";
 import { firebaseWriteTest } from "./firebase/firebase-test.js";
 
-gameInitFromStorage(); // GameState
 initI18n(); // Translation
 initPanelNavigation(); // Navigation
 await firebaseWriteTest(); // Firebase Test
