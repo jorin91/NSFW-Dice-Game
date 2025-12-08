1d507999df8188e5c82238cae03f99aaaa590451
jorin91 <jorin91@hotmail.com>
2025-12-08 11:32:27 +0100

    Make buildGameListElement function asynchronous to handle game list retrieval

--- js/panel-joingame.js
+++ js/panel-joingame.js
@@ -1,17 +1,21 @@
 // js/panel-joingame.js
+import { setI18n } from "./lang_i18n.js";
+import { listGames } from "./firebase/firebase-game.js";
 
 export function setupPanelJoinGame() {
   const joinButton = document.getElementById("panel-mainmenu.button.joinGame");
   if (joinButton) {
-    joinButton.addEventListener("click", (e) => {
-        buildGameListElement();
+    joinButton.addEventListener("click", async (e) => {
+      buildGameListElement();
     });
   }
 }
 
-function buildGameListElement() {
+async function buildGameListElement() {
   const body = document.getElementById("panel-joingame.body");
   if (!body) return;
 
+  body.innerHTML = "";
 
+  const gameIDList = await listGames();
 }
