69b5d542923f5ec49365b89858c2e7acf5110773
jorin91 <jorin91@hotmail.com>
2025-12-08 09:36:08 +0100

    Initialize tasks in GAMESTATE_MODEL using getTaskModel for dynamic task loading

--- js/gamestate.js
+++ js/gamestate.js
@@ -2,6 +2,7 @@
 import { deepCopy } from "./utils.js";
 import { storageSave, storageLoad, storageClear } from "./localstorage.js";
 import { dbUpdate, subscribeValue } from "./firebase/firebase-db.js";
+import { getTaskModel } from "./task.js";
 
 const LS_KEY_GAMESTATE = "NSFWDiceGame_GameState";
 
@@ -12,7 +13,7 @@ const GAMESTATE_MODEL = {
   gameID: null, // unieke game ID (6-cijferig)
   settings: null, // game-instellingen
   players: null, // spelerslijst
-  tasks: null, // takenlijst
+  tasks: await getTaskModel(), // takenlijst
   game: {
     currentRound: 0, // huidige ronde
     currentPlayerTurnIndex: 0, // index van speler die nu aan de beurt is
