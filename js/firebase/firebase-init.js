1be1dcd68b748f89c3adf25d72521ba4448685b7
jorin91 <jorin91@hotmail.com>
2025-12-08 08:00:00 +0100

    Import Firebase database methods in firebase-init.js

--- js/firebase/firebase-init.js
+++ js/firebase/firebase-init.js
@@ -1,6 +1,9 @@
+// js/firebase/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
 import { firebaseConfig } from "./firebase-config.js";
+import { ref, set, get } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseDB = getDatabase(firebaseApp);
+export { ref, set, get };
