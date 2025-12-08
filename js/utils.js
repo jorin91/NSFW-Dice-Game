98df659aa089001eb9913c226b3a0c23bb7be3d5
jorin91 <jorin91@hotmail.com>
2025-12-08 07:20:10 +0100

    Refactor utility functions for generating random IDs and numbers

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,19 @@ export function deepCopy(o) {
 export function generateRandomID() {
     return crypto.randomUUID();
 }
+
+export function generateTimestampID() {
+  return String(Date.now());
+}
+
+export function randomNumberString(length = 6) {
+  let num = Math.floor(Math.random() * 10 ** length).toString();
+  return num.padStart(length, "0");
+}
+
+export function randomNumber(length = 6) {
+  if (length <= 0) return 0;
+  const min = 10 ** (length - 1);
+  const max = 10 ** length;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
