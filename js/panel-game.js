586c23806bfedca9881582ef15610a0b22218501
jorin91 <jorin91@hotmail.com>
2025-12-08 09:56:47 +0100

    Add setupPanelGame function to initialize game header dynamically

---
+++ js/panel-game.js
@@ -0,0 +1,19 @@
+// js/panel-game.js
+import { setI18n } from "./lang_i18n.js";
+import { GAMESTATE } from "./gamestate.js";
+
+export function setupPanelGame() {
+  const header = document.getElementById("panel-game.header");
+  if (header) {
+    header.innerHTML = "";
+    const title = document.createElement("h2");
+    setI18n(
+      title,
+      "ui.panel-game.header",
+      { gameID: GAMESTATE.gameID },
+      "text",
+      true
+    );
+    header.appendChild(title);
+  }
+}
