4ae0f2ff4afd8805f2d47eeaf20f5aa8112c61ed
jorin91 <jorin91@hotmail.com>
2025-12-19 21:56:02 +0100

    Fix player panel setup by updating import and function call to use setupPanelPlayerOverview

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 import { initI18n } from "./lang_i18n.js";
 import { initPanelNavigation } from "./panelnavigation.js";
 import { firebaseWriteTest } from "./firebase/firebase-test.js";
-import { setupPanelPlayer, setupPanelPlayerSetup } from "./panel-player.js";
+import { setupPanelPlayerOverview, setupPanelPlayerSetup } from "./panel-player.js";
 import { setupPanelNewGame } from "./panel-newgame.js";
 import { set } from "./firebase/firebase-init.js";
 import { setupPanelJoinGame } from "./panel-joingame.js";
@@ -14,7 +14,7 @@ export async function init() {
 }
 
 export async function initUpdate() {
-  setupPanelPlayer(); // Setup Player Panel
+  setupPanelPlayerOverview(); // Setup Player Panel
   setupPanelPlayerSetup(); // Setup Player Setup Panel
   setupPanelNewGame(); // Setup New Game Panel
   setupPanelJoinGame(); // Setup Join Game Panel
