e82b3a6e265e0f2b8f23ab5c1b1e355f0a82ef18
jorin91 <jorin91@hotmail.com>
2025-12-20 17:53:36 +0100

    Add setupPanelGamePlay function for game play panel initialization

--- js/panel-game.js
+++ js/panel-game.js
@@ -27,3 +27,28 @@ export function setupPanelGameTask(id = "game-task") {
   // Build footer
   panel.footer.innerHTML = "";
 }
+
+export function setupPanelGamePlay(id = "game-play") {
+  let panel = getPanel(id);
+  if (!panel) panel = makePanel(id, true);
+
+  // Build header
+  panel.header.innerHTML = "";
+
+  const h2Header = document.createElement("h2");
+  setI18n(h2Header, "ui.panel-game-play.header");
+  panel.header.appendChild(h2Header);
+
+  const subHeader = document.createElement("span");
+  setI18n(subHeader, "{ui.game.gameID} | {ui.game.gameCode}", {
+    gameID: GAMESTATE.gameID || "{ui.game.loading}",
+    gameCode: GAMESTATE.gameCode || "{ui.game.loading}",
+  });
+  panel.header.appendChild(subHeader);
+
+  // Build body
+  panel.body.innerHTML = "";
+
+  // Build footer
+  panel.footer.innerHTML = "";
+}
